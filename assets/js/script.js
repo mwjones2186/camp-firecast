6f5e7244f77a2e374add08483ccfdee71a036c72
Jonsey <mwjones2186@gmail.com>
2022-06-17 23:59:40 -0700

    have a google map. can not figure out how to integrate with our search. still working on it.

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,7 +17,7 @@ function userInput(){
 
 function getWeather(city){
     console.log(city)
-    var geo = `http://api.openweathermap.org/geo/1.0/direct?q=${city},OR,US&appid=${api}`
+    var geo = `http://api.openweathermap.org/geo/1.0/direct?q=${city},US,US&appid=${api}`
  fetch(geo).then(function (results){
     return results.json()
  }).then(function(data){
@@ -31,6 +31,30 @@ var forecast = `https://api.openweathermap.org/data/2.5/onecall?lat=${data[0].la
      })
  }
  )
+}
+https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d1081910.6226718014!2d-123.13672913878243!3d42.04389353885103!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1sen!2sus!4v1655525107853!5m2!1sen!2sus" width="300" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
+function initMap() {
+    var 
+    var myLatLng = {lat: -25.363, lng: 131.044};
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 4,
+        center: myLatLng
+    });
+
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: 'Hello World!'
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+        alert('Marker clicked')
+    });
+
+    google.maps.event.addDomListener(map, "click", function() {
+        alert('Map clicked')
+    });
 }
 
 
