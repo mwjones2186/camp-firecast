650ad83e1c85c5e31ece4d2a9f723687b3505bb4
Jonsey <mwjones2186@gmail.com>
2022-06-19 18:40:42 -0700

    modified to reconcile code. will need to confirm functionality but believe the code is accurate and up to day.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,37 +3,32 @@ var api = '774222c77f8b3fa1592aeb44a0bae645'
 var cityName = document.getElementById('cityName')
 var searchBtn = document.getElementById('search')
 
-<<<<<<< HEAD
-=======
+
+
 // User input for city name
->>>>>>> 2b56af4b1ba3acdd14ad4e6d654365fe8437ea0b
+
 function userInput() {
     var city = cityName.value
     getWeather(city)
 }
 
-<<<<<<< HEAD
-function getWeather(city) {
-    console.log(city)
-    var geo = `http://api.openweathermap.org/geo/1.0/direct?q=${city},US,US&appid=${api}`
-=======
+
 // Geo locator API for city input by user
 function getWeather(city) {
 
     // console.log(city)
     var geo = `http://api.openweathermap.org/geo/1.0/direct?q=${city},OR,US&appid=${api}`
->>>>>>> 2b56af4b1ba3acdd14ad4e6d654365fe8437ea0b
+
     fetch(geo).then(function (results) {
         return results.json()
     }).then(function (data) {
         // console.log(data)
 
-<<<<<<< HEAD
-=======
+
+
         // Weather API retriever 
         //this is the weather info needs lat (latitude) and lon (longitude)
-        // api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
->>>>>>> 2b56af4b1ba3acdd14ad4e6d654365fe8437ea0b
+
         var forecast = `https://api.openweathermap.org/data/2.5/onecall?lat=${data[0].lat}&lon=${data[0].lon}&exclude=alerts,minutely,hourly,current&units=imperial&appid=${api}`
         fetch(forecast).then(function (results) {
             return results.json()
@@ -41,26 +36,8 @@ function getWeather(city) {
             console.log(data)
         })
 
-<<<<<<< HEAD
-        
-    });
 
 
-
-//     function initMap() {
-//         getWeather.then;
-//         const lat = data[0].lat
-//         const lon = data[0].lon
-
-//         const map = new google.maps.Map(document.getElementById("map"), {
-//           zoom: 0,
-//           center: uluru,
-//         });
-    
-//         window.initMap = initMap;
-};
-searchBtn.addEventListener('click', userInput)
-=======
         // Red Flag weather elements for Firecast report
         for (let i = 0; i < data.daily.length; i++) {
             let wind = data.daily[i].wind_speed;
@@ -94,4 +71,4 @@ searchBtn.addEventListener('click', userInput)
 };
 searchBtn.addEventListener('click', userInput)
 
->>>>>>> 2b56af4b1ba3acdd14ad4e6d654365fe8437ea0b
+
